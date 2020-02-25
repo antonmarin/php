97d72a477b56596069374797f84c104731ad8d81
Anton Marin <antonaryo85@gmail.com>
2020-02-25 23:53:01 +0300

    draft of fpm image

--- tests/test_fpm.sh
+++ tests/test_fpm.sh
@@ -4,6 +4,9 @@ set -eu
 echo '9000 port should be opened'
 nc -zv fpm 9000
 
+echo 'php-fpm config should be valid'
+php-fpm -t
+
 #echo -n '9000 port should be opened'
 #nc -zv fpm 9000
 #echo ' -> OK'
