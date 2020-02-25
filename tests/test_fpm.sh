e3c8982eccd56fe911ac6896e37fb5eecaa2b8c4
Anton Marin <antonaryo85@gmail.com>
2020-02-26 00:02:53 +0300

    xdebug should be enabled if extension is passed

--- tests/test_fpm.sh
+++ tests/test_fpm.sh
@@ -7,6 +7,6 @@ nc -zv fpm 9000
 echo 'php-fpm config should be valid'
 php-fpm -t
 
-#echo -n '9000 port should be opened'
-#nc -zv fpm 9000
-#echo ' -> OK'
+echo -n 'xdebug should be enabled if zend_extension=xdebug.so is passed to cmd'
+php-fpm -d zend_extension=xdebug.so -i | grep -q "xdebug support => enabled"
+echo ' -> OK'
